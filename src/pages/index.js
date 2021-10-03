1ac9af071abb493c0285a479c813239e5dd38c66
JuniorUyi <juniorjerry91@gmail.com>
2021-10-03 20:27:38 +0100

    Gatsby layouts implemented

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import styles from '../styles/styles.scss';
+
+// styles
+
+// data
+
+// markup
+const IndexPage = () => (
+  
+    <main>
+      <title>Home Page</title>
+      <h1>I'm the layout page</h1>
+    </main>
+  
+);
+
+export default IndexPage;
