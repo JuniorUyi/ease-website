167fe798e501a87170639c6d0e5c01d77bfd4f26
Anastasios Orfanidis <anastasios.orfanidis@gmail.com>
2021-10-11 18:03:13 +0100

    used grid for layout

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+const LayoutWrapper = styled.div`
+  min-height: 100vh;
+  margin: 0;
+
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+`;
+
 export default function Layout({ children }) {
   return (
-    <>
+    <LayoutWrapper>
       <Header />
       <main>{children}</main>
       <Footer />
-    </>
+    </LayoutWrapper>
   );
 }
